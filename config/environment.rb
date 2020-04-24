6b0edef6364839f3751032de81a877cb1057278c
ArunSarva <aramegowda@qwinix.io>
2020-04-24 12:33:51 +0530

    Email varification user sandgrid completed with API key

--- config/environment.rb
+++ config/environment.rb
@@ -2,14 +2,22 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-Rails.application.initialize!
+# Rails.application.initialize!
 
+#   ActionMailer::Base.smtp_settings = {
+#     address: 'smtp.sendgrid.net',
+#     port: 587,
+#     domain: 'google.com',
+#     user_name: ENV['SENDGRID_USERNAME'],
+#     password: ENV['SENDGRID_PASSWORD'],
+#     authentication: :login,
+#     enable_starttls_auto: true
+#   }
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    domain: 'google.com',
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    authentication: :login,
-    enable_starttls_auto: true
+    domain: 'google.COM',
+    address:        "smtp.sendgrid.net",
+    port:            587,
+    authentication: :plain,
+    user_name:      'apikey',
+    password:       ENV['SENDGRID_API_KEY']
   }
